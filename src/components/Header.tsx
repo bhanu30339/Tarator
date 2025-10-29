89861b8e31e77a30ef46afd5d1ce81d06f743fa0
bhanu30339 <bhanuprakashanumuladasu@gmail.com>
2025-10-29 10:57:44 +0530

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -78,7 +78,7 @@ export default function Header({ currentPage, onNavigate }: HeaderProps) {
                 <img
                   src={logo}
                   alt="Tarator Logo"
-                  className="h-19 w-auto"
+                  className="h-15 w-auto"
                   onError={handleLogoError}
                 />
               ) : (
